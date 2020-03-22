d628b51b9ba43090e2e89f5d7de7c04774f85806
Shivashankar <jeevan.shivashankar.ext@nokia.com>
2020-03-22 21:09:25 +0530

    changes are added

--- src/test/java/stepDefs/DocumentationPageStepDefs.java
+++ src/test/java/stepDefs/DocumentationPageStepDefs.java
@@ -1,5 +1,6 @@
 package stepDefs;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.apache.log4j.LogManager;
@@ -9,7 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class DocumentationPageStepDefs {
 
@@ -17,10 +20,6 @@ public class DocumentationPageStepDefs {
 
     public WebDriver driver;
     public static final Logger LOGGER = LogManager.getLogger(DocumentationPageStepDefs.class);
-    //WebDriverWait wait = new WebDriverWait(driver,10);
-
-
-
 
 
     @Given("^launch url for Here website \"([^\"]*)\"$")
@@ -42,6 +41,46 @@ public class DocumentationPageStepDefs {
 
     }
 
+    @And("^verify active Documentation link in Documentaion page$")
+    public void verifyActiveDocumentationLinkInDocumentaionPage() throws Throwable{
+
+        LOGGER.info("separate out the Documention links and verify");
+        List<WebElement> docLinks = driver.findElements(By.xpath("//span[@class='cta__content'][contains(text(),'Documentation')]"));
+        System.out.println(docLinks.size());
+        String title=null;
+        String parentWindow = driver.getWindowHandle();
+        String currentUrl = driver.getCurrentUrl();
+
+        for(int i=0; i<docLinks.size(); i++){
+            docLinks.get(i).click();
+            Thread.sleep(4000);
+            Set<String> allWindowHandles = driver.getWindowHandles();
+            Iterator<String> itr = allWindowHandles.iterator();
+
+            for(String handle : allWindowHandles)
+            {
+                System.out.println("Window handle - > " + handle);
+                driver.switchTo().window(handle);
+                Thread.sleep(4000);
+                title = driver.getTitle();
+                System.out.println("Page is active and title is: "+title);
+                break;
+
+            }
+            driver.switchTo().window(parentWindow);
+        }
+        driver.get(currentUrl);
+    }
+
+    @Then("^close the browser$")
+    public void closeTheBrowser() throws Throwable{
+
+        LOGGER.info("Closing all the browser ");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+
 
     public void navigateToDocumentPage() throws Throwable{
 
@@ -50,13 +89,5 @@ public class DocumentationPageStepDefs {
         driver.findElement(By.xpath("//li[@class='menu-list-item menu-list-item--primary has-flyout binded']" +
                 "//a[@class='menu-link menu-link--primary']//span[@class='link-content'][contains(text(),'Documentation')]")).click();
         Thread.sleep(5000);
-    }
-
-    @Then("^verify active Documentation link in Documentaion page$")
-    public void verifyActiveDocumentationLinkInDocumentaionPage() {
-
-        LOGGER.info("separate out the Documention links and verify");
-        List<WebElement> docLinks = driver.findElements(By.linkText("a"));
-        System.out.println(docLinks);
     }
 }
--- src/test/resources/Documentation.feature
+++ src/test/resources/Documentation.feature
@@ -1,6 +1,7 @@
-Feature: document
+Feature: document link Verification in Doc Page
 
   Scenario: Verify the Documentation link in Here website page
     Given launch url for Here website "https://developer.here.com/"
     Then Navigate to Documentation page
-    Then verify active Documentation link in Documentaion page
+    And verify active Documentation link in Documentaion page
+    Then close the browser
